5389d036aaa5627db6737fb83ffd77f34d646a91
Dylan de Wit <dylan.de.wit@profit4cloud.nl>
2022-06-08 09:41:22 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'profit4cloud-kennisapp',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
